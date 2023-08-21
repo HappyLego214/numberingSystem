62902a28f3e54e727c19f7b9d721273ae003fefa
HappyLego214 <anthony21josh@gmail.com>
2023-08-21 16:56:53 +0800

    adds hex to decimal, binary to hex,  and decimal to hex

--- scripts.js
+++ scripts.js
@@ -6,10 +6,15 @@ const binaryConvert = document.querySelector("#binaryConvert");
 const decimalConvert = document.querySelector("#decimalConvert");
 const hexaConvert = document.querySelector("#hexaConvert");
 
+const hexAlphaArr = ["A","B","C","D","E","F"];
+const hexNumericArr = [10,11,12,13,14,15];
+const hexBinaryArr = [1010,1011,1100,1101,1110,1111];
+
 binaryConvert.addEventListener("click", () => {
     if (binaryInput.validity.valid) {
         console.log("Valid!");
         binaryToDecimal(Array.from(binaryInput.value));
+        binaryToHex(Array.from(binaryInput.value));
     } else {
         console.log("Invalid");
     }
@@ -28,7 +33,7 @@ decimalConvert.addEventListener("click", () => {
 hexaConvert.addEventListener("click", () => {
     if(hexaInput.validity.valid) {
         console.log("Valid!");
-        decimalToHex(hexaInput.value);
+        hexaToDecimal(Array.from(hexaInput.value));
     } else {
         console.log("Invalid");
     }
@@ -43,6 +48,30 @@ function binaryToDecimal(binaryArray) {
     }
     
     updateDecimal(sum);
+}
+
+function binaryToHex(binaryArray) {
+    let hexaArray = [];
+    binaryArray.reverse();
+    do {
+        if(binaryArray.length > 3) {
+            let partition = binaryArray.splice(0,4).reverse().join("");
+            let index = hexBinaryArr.findIndex((item) => item == partition);
+            hexaArray.push(hexAlphaArr[index]);
+            console.log('check-1');
+        } else {
+            let sum = 0;
+            for(i = 0; i < binaryArray.length; i++)
+            {
+                sum += binaryArray[i] * Math.pow(2, i);
+            }
+            hexaArray.push(sum);
+            break;
+        }
+    } while (binaryArray.length != 0);
+
+
+    updateHexa(hexaArray.reverse().join(''));
 }
 
 function decimalToBinary(decimalValue) {
@@ -64,9 +93,7 @@ function decimalToHex(decimalValue) {
         let remainder = decimal % 16;
         console.log(remainder);
         if (remainder > 9 ) {
-            const hexAlphaArr = ["A","B","C","D","E","F"];
-            const hexNumericArr = [10,11,12,13,14,15];
-            let index = hexNumericArr.findIndex((item) => item === remainder );
+            let index = hexNumericArr.findIndex((item) => item == remainder);
             hexaArray.push(hexAlphaArr[index]);
         } else {
             hexaArray.push(remainder);
@@ -77,6 +104,26 @@ function decimalToHex(decimalValue) {
 
     hexaArray.reverse();
     updateHexa(hexaArray.join(''));
+}
+
+function hexaToDecimal(hexaValue) {
+    let sum = 0;
+    hexaValue.reverse();
+    for (i = 0; i < hexaValue.length; i++) {
+        if(parseInt(hexaValue[i])) {
+            sum += hexaValue[i] * Math.pow(16, i);
+        } else {
+            let index = hexAlphaArr.findIndex((item) => item == hexaValue[i]);
+            let hexNum = hexNumericArr[index];
+            sum += hexNum * Math.pow(16, i);      
+        }
+    }
+
+    updateDecimal(sum);
+}
+
+function hexaToBinary(hexaValue) {
+    
 }
 
 function updateDecimal(decimalConverted) {
